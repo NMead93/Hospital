efac11c06fb8be49cc7a388ef0d8636c724eccd2
Nicholas Mead and Shruti Priya <nicholas+shruti@>
2017-02-21 11:59:19 -0800

    get the initial setup for the project and create a patient table with all the required data and save the values in the database

---
+++ Objects/Patient.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace Hospital.Objects
+{
+  public class Patient
+  {
+    private int _id;
+    private string _name;
+    private string _birthday;
+    private int doctor_id;
+
+    public Patient (string name, string birthday, int doctorId,int Id=0)
+    {
+      _id = Id;
+      _name = name;
+      _birthday = birthday;
+      doctor_id = doctorId;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void  SetName(string name)
+    {
+      _name = name;
+    }
+    public string GetBirthday()
+    {
+      return _birthday;
+    }
+    public void  SetBirthday(string birthday)
+    {
+      _birthday = birthday;
+    }
+    public int GetDoctorId()
+    {
+      return doctor_id;
+    }
+    public void SetDoctorId(int doctorId)
+    {
+      doctor_id = doctorId;
+    }
+
+    public override bool Equals(System.Object otherPatient)
+    {
+      if (!(otherPatient is Patient))
+      {
+        return false;
+      }
+      else
+      {
+        Patient newPatient = (Patient) otherPatient;
+        bool idEquality = (this.GetId()== newPatient.GetId());
+
+        return (idEquality);
+      }
+    }
+    public static List<Patient> GetAll()
+    {
+      List<Patient> allPatients = new  List<Patient>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM patients;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while (rdr.Read())
+      {
+        int patientId = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        string birthday = rdr.GetString(3);
+        int doctorId = rdr.GetInt32(2);
+        Patient newPatients = new Patient(name, birthday, doctorId, patientId);
+        allPatients.Add(newPatients);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allPatients;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO patients (name, doctor_id, birthday) OUTPUT INSERTED.id VALUES ( @PatientName,@PatientDoctorId,@PatientBirthday);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@PatientName";
+      nameParameter.Value = this.GetName();
+
+      SqlParameter birthdayParameter = new SqlParameter();
+      birthdayParameter.ParameterName = "@PatientBirthday";
+      birthdayParameter.Value = this.GetBirthday();
+
+      SqlParameter patientDoctorIdParameter = new SqlParameter();
+      patientDoctorIdParameter.ParameterName = "@PatientDoctorId";
+      patientDoctorIdParameter.Value = this.GetDoctorId();
+
+      cmd.Parameters.Add(nameParameter);
+      cmd.Parameters.Add(birthdayParameter);
+      cmd.Parameters.Add(patientDoctorIdParameter);
+
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM patients;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+
+  }
+
+}
---
+++ Tests/PatientTest.cs
@@ -0,0 +1,69 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Hospital.Objects;
+
+namespace Hospital
+{
+  public class PatientTest : IDisposable
+  {
+    public PatientTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Hospital_test;Integrated Security=SSPI;";
+    }
+
+    public void Dispose()
+    {
+      Patient.DeleteAll();
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+      int result = Patient.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_EqualOverrideTrueIfDescriptionAreTheSame()
+    {
+      Patient firstPatient = new Patient("nick", "June 25 1993", 2,1);
+      Patient secondPatient = new Patient("shruti", "October 8 1988", 3,1);
+
+      Assert.Equal(firstPatient, secondPatient);
+    }
+
+    [Fact]
+    public void  Test_Save_SavesToDatabase()
+    {
+     //Arrange
+      Patient testPatient = new Patient ("John", "April 1 1945", 5);
+
+      //Act
+      testPatient.Save();
+      List<Patient> result = Patient.GetAll();
+      List<Patient> testList = new List<Patient>{testPatient};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToObject()
+    {
+      //Arrange
+      Patient testPatient = new Patient ("John", "April 1 1954", 5);
+      //Act
+      testPatient.Save();
+      Patient savedPatient = Patient.GetAll()[0];
+
+      int result = savedPatient.GetId();
+      int testId = testPatient.GetId();
+
+      //Assert
+      Assert.Equal(testId,result);
+    }
+  }
+}
